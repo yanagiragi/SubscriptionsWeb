f4f1c82b283a079301f6c8e7f69f9192a414a2f7
yanagiragi <qaz90473@gmail.com>
2018-04-08 09:29:17 -0700

    refactor readAll

--- main.js
+++ main.js
@@ -12,7 +12,10 @@ const filepath = '../RagiSubscription/data/container.json'
 function read(data, str)
 {
 	console.log("read <" + str + ">");
+	
+	// dirtybit
 	var flag = false
+	
 	for(var site in data){
 		for(thread in data[site].containerList){
 			if(data[site].containerList[thread].title == str){
@@ -25,9 +28,60 @@ function read(data, str)
 			}
 		}
 	}
-	console.log('Saving container to file...')
-	fs.writeFileSync(filepath, JSON.stringify(data, null, 4), 'utf8');
-	console.log('Saving Done.')
+
+	if(flag){
+		console.log('Saving container to file...')
+		fs.writeFileSync(filepath, JSON.stringify(data, null, 4), 'utf8');
+		console.log('Saving Done.')
+	}
+	return flag
+}
+
+function noticed(data)
+{
+	data.isNoticed = true
+	return data
+}
+
+function readNoSave(data, str)
+{
+	console.log("read <" + str + ">");
+	
+	for(var site in data){
+		for(thread in data[site].containerList){
+			if(data[site].containerList[thread].title == str){
+				if(!(data[site].containerList[thread].isNoticed)){
+					// force search all every time
+					// because there are always duplicated title all the time
+					data[site].containerList[thread].isNoticed = true;
+				}
+			}
+		}
+	}
+	
+	return data
+}
+function readAll(data, str)
+{
+	console.log("readAll <" + str + ">")
+	
+	list = data[str].containerList.reduce((acc, cur) => {
+		if(!cur.isNoticed)
+			return acc.concat(cur)
+		return acc
+	}, [])
+
+	// dirtybit
+	var flag = list.length > 0
+
+	if(flag){
+		
+		data[str].containerList.map(x => noticed(x))
+
+		console.log('Saving container to file...')
+		fs.writeFileSync(filepath, JSON.stringify(data, null, 4), 'utf8');
+		console.log('Saving Done.')
+	}
 	return flag
 }
 
@@ -39,6 +93,15 @@ app.get('/', (req, res) => {
 app.get('/json', (req, res) => {
 	data = JSON.parse(fs.readFileSync(filepath, 'utf8'))
 	res.send(data)
+})
+
+app.get('/readAll/:title', (req, res) => {
+	
+	title = req.params.title
+	
+	data = JSON.parse(fs.readFileSync(filepath, 'utf8'))
+	result = readAll(data, title)
+	res.send(result)
 })
 
 app.get('/read/:title', (req, res) => {
