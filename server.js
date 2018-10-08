991e11c39881dbade2b008e7f8f5e7d134dd5485
yanagiragi <qaz90473@gmail.com>
2018-10-08 16:43:34 -0700

    modified implementation of /json

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const cors = require('cors')
 const fs = require('fs')
-const RagiDB = require('../../RagiDB/api.js')
+const RagiDB = require('../RagiDB/api.js')
 
 const app = express()
 app.use(cors())
 app.use(express.static('public'));
 app.listen(3001)
+console.log("RagiSubscriptionWeb Start")
 
-const filepath = '../RagiSubscription/data/container.json'
+const filepath = '../RagiDB/data/container.json'
 
 app.get('/', (req, res) => {
 	data = fs.readFileSync('index.html', 'utf8')
@@ -16,7 +17,20 @@ app.get('/', (req, res) => {
 })
 
 app.get('/json', (req, res) => {
-	RagiDB.GetContainer().then(data => res.send(data))
+	
+	// RagiDB.GetContainer().then(data => res.send(data))
+	
+	let data = JSON.parse(fs.readFileSync(filepath, 'utf8'))
+
+	data.container.map( e => {
+		e.list = e.list.filter(e2 => !e2.isNoticed)
+	})
+
+	res.send(data)
+})
+
+app.get('/jsonAll', (req, res) => {
+	res.send(JSON.parse(fs.readFileSync(filepath, 'utf8')))
 })
 
 app.get('/readAll/:title', (req, res) => {	
