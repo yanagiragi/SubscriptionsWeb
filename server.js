a9518508b8d443e45c321187b8881062c80c91c5
yanagiragi <qaz90473@gmail.com>
2018-12-05 23:50:05 +0800

    fix ip valiation and try to force GC

--- server.js
+++ server.js
@@ -14,15 +14,19 @@ const filepath = '../RagiDB/data/container.json'
 app.use(function(req, res, next){
 	
 	let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-	console.log(`Request from ${ip}, path = ${req.path}`)
+	if(ip && req.path){
+		console.log(`Request from ${ip}, path = ${req.path}`)
+	}
+	else{
+		console.log(`Request from ????, path = ${req.path}`)
+	}
 	next()
 })
 
 app.get('/', (req, res) => {
-
 	data = fs.readFileSync('index.html', 'utf8')
 	res.send(data)
-
+	data = null
 })
 
 app.get('/json', (req, res) => {
@@ -32,32 +36,40 @@ app.get('/json', (req, res) => {
 	data.container.map( e => {
 		e.list = e.list.filter(e2 => !e2.isNoticed)
 	})
-
 	res.send(data)
+	data = null
 })
 
 app.get('/jsonAll', (req, res) => {
 
-	res.send(JSON.parse(fs.readFileSync(filepath, 'utf8')))
-
+	let data = JSON.parse(fs.readFileSync(filepath, 'utf8'))
+	res.send(data)
+	data = null
 })
 
 app.get('/readAll/:title', (req, res) => {	
 	
-	entryIds = req.params.title.split('&')
+	let entryIds = req.params.title.split('&')
 	containerId = entryIds[0]
-	entryIds.splice(0,1)
+	entryIds.splice(0, 1)
+	// dont try to use let [containerId, entryIds] = ... here
+	// you may need to modified RagiDB API 
 	
 	console.log(`readAll ${containerId} : ${entryIds}`)
 	
 	RagiDB.NoticeEntryAll(containerId, entryIds).then(()=> res.send('DONE'))
 
+	entryIds = null
+	containerId = null
 })
 
 app.get('/read/:title', (req, res) => {
 	
-	[containerId, entryId] = req.params.title.split('|')
+	let [containerId, entryId] = req.params.title.split('|')
 	
 	RagiDB.NoticeEntry(containerId, entryId).then(()=> res.send('DONE'))
+	
+	entryId = null
+	containerId = null
 
 })
