1f743c349a31462536e46dbd7996ca82b32d4753
yanagiragi <qaz90473@gmail.com>
2021-03-28 09:11:03 +0800

    Change Add New Btn Layout & Add eslint rules

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ const PASSWORD = process.env.RSW_PASSWORD || 'PASSWORD'
 const PORT = 3007
 const COOKIE_NAME = Math.random()
 
-const filePath = '../DB/data/container.json'
-const dataPath = '../Crawler/data/data.json'
+const dataPath = '../Subscriptions/data/data.json'
 
 const indexPath = path.join(__dirname, 'html', 'index.html')
 const managePath = path.join(__dirname, 'html', 'manage.html')
@@ -32,16 +31,15 @@ app.listen(PORT)
 
 console.log('RagiSubscriptionWeb Start, TOKEN = ' + TOKEN)
 
-app.use(function (req, res, next) {
-	let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+app.use(async function (req, res, next) {
+	const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
 
-	console.log(`Request from ${ip ? ip : '???'}, path = ${req.path}, req.cookies = ${JSON.stringify(req.cookies)}`)
+	console.log(`Request from ${ip || '???'}, path = ${req.path}, req.cookies = ${JSON.stringify(req.cookies)}`)
 
 	const whiteList = ['/login', '/favicon.ico']
-	if(whiteList.includes(req.path) || (req.cookies && req.cookies[COOKIE_NAME] === TOKEN)) {
+	if (whiteList.includes(req.path) || (req.cookies && req.cookies[COOKIE_NAME] === TOKEN)) {
 		next()
-	}
-	else {
+	} else {
 		res.redirect('/login')
 	}
 })
@@ -51,21 +49,19 @@ app.get('/', (req, res) => {
 })
 
 app.get('/login', (req, res) => {
-	if(req.signedCookies && req.signedCookies.auth === TOKEN) {
+	if (req.signedCookies && req.signedCookies.auth === TOKEN) {
 		res.redirect('/')
-	}
-	else {
+	} else {
 		res.sendFile(loginPath)
 	}
 })
 
 app.post('/login', (req, res) => {
-	if(req.body.password === PASSWORD){
-		//res.cookie('auth', TOKEN, { signed: true, maxAge: 1000 * 60 * 60 * 24 * 365 })
+	if (req.body.password === PASSWORD) {
+		// res.cookie('auth', TOKEN, { signed: true, maxAge: 1000 * 60 * 60 * 24 * 365 })
 		res.cookie(COOKIE_NAME, TOKEN, { maxAge: 1000 * 60 * 60 * 24 * 365 })
 		res.redirect('/')
-	}
-	else {
+	} else {
 		res.redirect('/login')
 	}
 })
@@ -89,7 +85,7 @@ app.get('/jsonAll', async (req, res) => {
 })
 
 app.get('/readAll/:title', async (req, res) => {
-	let listIds = req.params.title.split('&')
+	const listIds = req.params.title.split('&')
 	console.log(`readAll : ${listIds}`)
 	const args = { listIds }
 	const result = await DbApi.NoticeEntryAll(args)
@@ -104,13 +100,13 @@ app.get('/read/:title', async (req, res) => {
 })
 
 app.get('/manage', (req, res) => {
-	let data = fs.readFileSync(managePath, 'utf-8')
+	const data = fs.readFileSync(managePath, 'utf-8')
 	res.send(data)
 })
 
 app.get('/data', (req, res) => {
 	try {
-		let data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'))
+		const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'))
 		res.send(data)
 	} catch (e) {
 		res.send('Error')
@@ -119,19 +115,25 @@ app.get('/data', (req, res) => {
 
 app.post('/save', (req, res) => {
 	try {
-		let data = req.body
+		const data = req.body
 
-		let isInvalid = 0
+		const Invalids = []
 
 		for (let i = 0; i < data.length; ++i) {
-			isInvalid += data[i].sites.reduce((acc, ele) => {
-				if (ele.url === 'NULL' || ele.nickname === 'NULL') { return acc.concat(ele) } else { return acc }
-			}, []).length > 0
+			const invalideSites = data[i].sites.reduce((acc, ele) => {
+				const hasError = ele.url === 'NULL' ||
+								ele.nickname === 'NULL' ||
+								ele.url.length === 0 ||
+								ele.nickname.length === 0
+				return hasError ? acc.concat(ele) : acc
+			}, [])
+
+			if (invalideSites.length > 0) { Invalids.push(invalideSites) }
 		}
 
-		console.log(`isInvalid = ${isInvalid}`)
+		console.log(`isInvalid = ${JSON.stringify(Invalids)}`)
 
-		if (isInvalid) {
+		if (Invalids.length > 0) {
 			res.send('NO')
 		} else {
 			fs.writeFileSync(dataPath, JSON.stringify(data, null, 4), 'utf8')
